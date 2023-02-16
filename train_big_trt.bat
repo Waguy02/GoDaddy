1bfea390cec413be9031ebae63f1ed9566e5bf42
waguy <guywaffo@gmail.com>
2023-02-16 06:52:21 +0100

    "Updating model config"

--- train_big_trt.bat
+++ train_big_trt.bat
@@ -1,2 +1,2 @@
 python run_training_transformer.py -lr=0.001 -bs=256 -c -dv -de=64 -nl=6 -nh=8 -df=512 -sl=25 -e=700 -w0
-ECHO python run_training_transformer.py -lr=0.001 -bs=256 -c -dv -de=64 -nl=6 -nh=8 -df=256 -sl=25 -e=700 -w8
+python run_training_transformer.py -lr=0.001 -bs=256 -c -dv -de=64 -nl=6 -nh=8 -df=256 -sl=25 -e=700 -w8
